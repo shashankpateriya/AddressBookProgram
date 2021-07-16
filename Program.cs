a61efa3d353c374ea77a5df80a8f4150994062c6
Shashank <shashankpateriya4@gmail.com>
2021-07-16 13:03:22 +0530

    [shashank] Add: Address Book System Main Program

--- Program.cs
+++ Program.cs
@@ -7,24 +7,42 @@ namespace AddressBook
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Address Book Program");
+            AddressBookDetails abd = new AddressBookDetails();
 
             while (true)
             {
+
+                Console.WriteLine("*********************************************************");
                 Console.WriteLine("1.Add member to Contact list \n2.View Members in Contact List\n3.Edit members Contacts list\n4.Delete members Contacts list\n5.Exit");
                 Console.WriteLine("Enter an option:");
                 switch (Convert.ToInt32(Console.ReadLine()))
                 {
                     case 1:
-                        UC5AddMultipleContacts.AddMember();
+                        abd.AddMember();
+                        //UC1CreateAddressBook.AddMember();
+                        //UC2AddContacts.AddMember();
+                        //UC3EditContact.AddMember();
+                        //UC4DeleteContact.AddMember();
+                        //UC5AddMultipleContacts.AddMember();
                         break;
                     case 2:
-                        UC5AddMultipleContacts.ViewContacts();
+                        abd.ViewContacts();
+                        //UC1CreateAddressBook.ViewContacts();
+                        //UC2AddContacts.ViewContacts();
+                        //UC3EditContact.ViewContacts();
+                        //UC4DeleteContact.ViewContacts();
+                        //UC5AddMultipleContacts.ViewContacts();
                         break;
                     case 3:
-                        UC5AddMultipleContacts.EditDetails();
+                        abd.EditDetails();
+                        //UC3EditContact.EditDetails();
+                        //UC4DeleteContact.EditDetails();
+                        //UC5AddMultipleContacts.EditDetails();
                         break;
                     case 4:
-                        UC5AddMultipleContacts.DeleteDetails();
+                        abd.DeleteDetails();
+                        //UC4DeleteContact.DeleteDetails();
+                        /UC5AddMultipleContacts.DeleteDetails();
                         break;
                     case 5:
                         // to exit from main method
@@ -32,6 +50,7 @@ namespace AddressBook
                         return;
 
                 }
+
             }
         }
     }
